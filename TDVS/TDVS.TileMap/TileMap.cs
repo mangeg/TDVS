0c6e5ccaadcd8ab5efb458d9efd7c90ba8320812
Mange <>
2011-09-03 21:56:12 +0200

    - Renaming TDVS.Common.TileMap -> TDVS.TileMap.c

--- TDVS/TDVS.TileMap/TileMap.cs
+++ TDVS/TDVS.TileMap/TileMap.cs
@@ -9,7 +9,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
-namespace TDVS.Common.TileMap
+namespace TDVS.TileMap
 {
     public class TileMap
     {
